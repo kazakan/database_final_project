64856d2e4f7077ca1185476ab0942970190254e6
Jh Do <dojh99@gmail.com>
2022-05-29 18:08:25 +0900

    Fix : applied async, multiprocessing

--- crawl/crawl.py
+++ crawl/crawl.py
@@ -1,66 +1,76 @@
+import asyncio
+from time import time
 from typing import List
 import requests
 from bs4 import BeautifulSoup
+from multiprocessing import Pool
 
-def getMovieDetailPage(code):
-    result = requests.get(f"https://movie.naver.com/movie/bi/mi/detail.naver?code={code}")
-    if(result.status_code != 200) : return None
-    if("영화 코드값 오류입니다" in result.text): return None
-    return result.text
+async def getMovieDetailPage(code):
+    result = await loop.run_in_executor(None, requests.get,f"https://movie.naver.com/movie/bi/mi/detail.naver?code={code}")
+    return result
 
 def parseMyInfo(pageSoup : BeautifulSoup):
-    soup_my_info : BeautifulSoup = pageSoup.select_one('div.mv_info:nth-child(1)')
+    soup_my_info : BeautifulSoup = pageSoup.select_one('.mv_info_area > .mv_info')
 
     ret = {}
 
-    # movie name
-    ret['name'] = soup_my_info.select_one('h3:nth-child(1) > a:nth-child(1)').text
+    if(soup_my_info is None) : # this need login
+        return None
 
-    str_subhead : str = soup_my_info.find("strong","h_movie2").text
+    # movie name
+    ret['name'] = soup_my_info.select_one('h3:nth-child(1) > a').text
+
+    str_subhead : str = soup_my_info.find("strong","h_movie2")['title']
     
     # movie alter name, year
-    pos_comma : int = str_subhead.rfind(',')
-    ret['altname'] = None if pos_comma == -1 else str_subhead[:pos_comma]
-    year = str_subhead if pos_comma == -1 else str_subhead[pos_comma+1:]
-    ret['year'] = int(year)
+    if len(str_subhead) > 0:
+        pos_comma : int = str_subhead.rfind(',')
+        ret['altname'] = None if pos_comma == -1 else str_subhead[:pos_comma]
+        year = str_subhead if pos_comma == -1 else str_subhead[pos_comma+1:]
+        ret['year'] = int(year)
 
 
     # parse from info_spec
     soup_info_spec : BeautifulSoup = soup_my_info.find("dl","info_spec")
 
     # first dd in infospec
-    # summary | nation | minute (optional) | release (optional)
+    # genres | nation | minute (optional) | release (optional)
     dd1 : BeautifulSoup = soup_info_spec.select_one("dd:nth-child(2)")
     dd1_spans : List[BeautifulSoup] = dd1.find_all("span")
 
-    # summary
-    a_summary : List[BeautifulSoup] = dd1_spans[0].find_all("a")
-    ret['summarys'] = [t.text for t in a_summary]
-
-    # country
-    ret['country']  = dd1_spans[1].find("a").text
-
-    # minute
-    if(len(dd1_spans) >= 3) :
-        ret['minute'] = dd1_spans[2].text.strip()[:-1]
-
-    # release date
-    if(len(dd1_spans) >= 4) :
-        ret["date"] = dd1_spans[3].find_all("a")[-1].text.strip('.')
+    def parseSpan(span : BeautifulSoup):
+        if("N=a:ifo.genre" in span.text): # genre
+            a_summary : List[BeautifulSoup] = span.find_all("a")
+            ret['summarys'] = [t.text for t in a_summary]
+        elif("N=a:ifo.nation" in span.text): #nation
+            a_country = span.find("a")
+            ret['country']  = a_country.text if(a_country) else None
+        elif("N=a:ifo.nation" in span.text): #nation
+            ret['minute'] = span.text.strip()[:-1]
+        elif("N=a:ifo.day" in span.text): # release date
+            ret["date"] = span.find_all("a")[-1].text.strip('.')
+    
+    for span in dd1_spans:
+        parseSpan(span)
         
     return ret
 
 def getDirectors(pageSoup : BeautifulSoup) :
     ret = []
     div_director : BeautifulSoup = pageSoup.select_one(".director")
+
+    if(div_director is None) : return ret # not available
+    
     divs_dir_obj : List[BeautifulSoup] = div_director.select("div.dir_obj")
     for dir_obj in divs_dir_obj :
+        data = {}
+        
         dir_obj_a = dir_obj.select_one(".dir_product > a:nth-child(1)",href=True)
         href = dir_obj_a['href']
-        dir_name = dir_obj_a['title']
+        data['name'] = dir_obj_a['title']
 
-        dir_code = href.split("=")[-1].strip()
-        ret.append((dir_code,dir_name))
+        data['code'] = int(href.split("=")[-1].strip())
+        ret.append(data)
 
     return ret
 
@@ -74,36 +84,78 @@ def getActors(pageSoup : BeautifulSoup) :
     divs_p_info : List[BeautifulSoup] = div_made_people.select(".p_info")
 
     for p_obj in divs_p_info :
+        data = {}
         p_obj_a = p_obj.select_one("a:nth-child(1)",href=True)
         href = p_obj_a['href']
 
         # actor name, code, main or sub actor, role
-        actor_name = p_obj_a['title']
-        actor_code = href.split("=")[-1].strip()
-        actor_ismain = p_obj.select_one("div:nth-child(3) > p:nth-child(1) > em:nth-child(1)").text
+        data['name'] = p_obj_a['title']
+        data['code'] = int(href.split("=")[-1].strip())
+        data['ismain'] = p_obj.select_one("div:nth-child(3) > p:nth-child(1) > em:nth-child(1)").text
         span_actor_role = p_obj.select_one("div:nth-child(3) > p:nth-child(2) > span:nth-child(1)")
-        actor_role = span_actor_role.text if span_actor_role else None
+        data['role'] = span_actor_role.text if span_actor_role else None
 
-        ret.append((actor_code,actor_name,actor_ismain,actor_role))
+        ret.append(data)
 
     return ret
 
+def crawlOnePage(page : BeautifulSoup):
+    result = {}
+
+    if(page is None):
+        return None
+
+    movie = parseMyInfo(page)
+    if(movie is None) :
+            return result
+    result['mv'] = movie
+    result['director'] = getDirectors(page)
+    result['actor'] = getActors(page)
+    return result
+
+def parseOne(response):
+        param = response
+        if(param.status_code != 200) : return None
+        elif("영화 코드값 오류입니다" in param.text): return None
+        
+        bs = BeautifulSoup(param.text,'html.parser')
+        
+        result = None
+        try:
+            result = crawlOnePage(bs)
+        except:
+            return None
+        return result
+
+async def main():
+    codes = range(10000,20000)
+    begin = time()
+    responses_future = [asyncio.ensure_future(getMovieDetailPage(code)) for code in codes]
+    responses = await asyncio.gather(*responses_future)
+    end = time()
+
+    print('다운 실행 시간: {0:.3f}초'.format(end - begin))
+
     
+
+    begin = time()
+    with Pool() as p:
+        ret = p.map(parseOne,responses)
+    end = time()
+
+    print('Parsing 실행 시간: {0:.3f}초'.format(end - begin))
+
+    return ret
+
 
 if __name__ == "__main__":
 
-    for code in [80,182016,99481,24452]:
-        html = getMovieDetailPage(code)
-        if(html is None):
-            print(None)
-            continue
-        bs = BeautifulSoup(html,'html.parser')
-        reslt = parseMyInfo(bs)
-        dirs = getDirectors(bs)
-        actors = getActors(bs)
-        print(reslt)
-        print(dirs)
-        print(actors)
+    
+    loop = asyncio.get_event_loop()         
+    ret = loop.run_until_complete(main())          
+    loop.close()                             
+    
+    #print(ret)
     
     
     
