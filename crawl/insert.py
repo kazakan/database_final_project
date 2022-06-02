4bc6daa81869fc851cf5658c2251608f9ae3445c
Jh Do <dojh99@gmail.com>
2022-06-02 22:48:36 +0900

    FEAT : Add db connection initiation

--- crawl/insert.py
+++ crawl/insert.py
@@ -1,6 +1,29 @@
 from glob import iglob
 import pickle
 from pathlib import Path
+import dotenv
+import os
+import pymysql
+
+def init_db():
+    dotenv.load_dotenv()
+    SQL_URL=os.getenv("SQL_URL")
+    SQL_DB=os.getenv("SQL_DB")
+    SQL_USER=os.getenv("SQL_USER")
+    SQL_PASSWD=os.getenv("SQL_PASSWD")
+
+    db = pymysql.connect(
+        user=SQL_USER,
+        passwd=SQL_PASSWD,
+        host=SQL_URL,
+        db=SQL_DB,
+        charset='utf8'
+    )
+
+    cursor = db.cursor()
+
+    return db, cursor
+
 
 def main():
     PICKLE_PATH_ROOT = Path("./out")
