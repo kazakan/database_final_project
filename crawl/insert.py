6cf51af05f6eff203cc944551c5e3fa5e5f315f2
Jh Do <dojh99@gmail.com>
2022-06-02 23:24:11 +0900

    FIX : Add insert functions skeleton

--- crawl/insert.py
+++ crawl/insert.py
@@ -1,9 +1,20 @@
 from glob import iglob
 import pickle
 from pathlib import Path
+from typing import Tuple
 import dotenv
 import os
 import pymysql
+
+MOVIE_COLUMNS =[
+    'mv_code',"mv_name","altname" ,"playtime" ,"release_date" ,
+    "watched_rating_num" ,"watched_rating" ,"commentor_rating" ,"netizen_rating_num" ,"netizen_rating" ,
+    "director_short" ,"country_short","actor_short","grade_short" ,"poster_url","year"
+]
+
+ACTOR_COLUMNS =["ac_code", "ac_name", "ismain", "ac_role", "img_url"]
+
+DIRECTOR_COLUMNS =["dr_code", "dr_name", " img_url"]
 
 def init_db():
     dotenv.load_dotenv()
@@ -23,6 +34,39 @@ def init_db():
     cursor = db.cursor()
 
     return db, cursor
+
+def insert_movie_param(code,data) -> Tuple or None :
+    mv = data['mv']
+    return (
+        code, # mv_code
+        mv.get("name"),        # "mv_name"
+        mv.get("altname"),        # "altname" 
+        mv.get("minute"),        # "playtime" 
+        None,        # "release_date" 
+        mv.get("watched_rating_num"),        #"watched_rating_num" 
+        mv.get("watched_rating"),         #"watched_rating" 
+        mv.get("commentor_rating"),         #"commentor_rating" 
+        mv.get("netizen_rating_num"),         #"netizen_rating_num" 
+        mv.get("netizen_rating"),         #"netizen_rating" 
+        mv.get("director_short"),         #"director_short" 
+        None,         #"country_short"
+        None,         #"actor_short"
+        None,         #"grade_short" 
+        data.get['poster'],         #"poster_url"
+        mv.get("year"),         #"year"
+    )
+
+def insert_actor_param(code,data)  ->  Tuple or None :
+    return ()
+
+def insert_director_param(code,data) -> Tuple or None :
+    return ()
+
+def insert_movie_param(code,data) -> Tuple or None :
+    return ()
+
+def insert_movie_param(code,data) -> Tuple or None :
+    return ()
 
 
 def main():
