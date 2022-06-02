2dafb45bfbf0b8a7b8b7fccec96fe2cb5ef4855f
Jh Do <dojh99@gmail.com>
2022-06-02 23:24:54 +0900

    REFACTOR : Frmat code with autopep8

--- crawl/insert.py
+++ crawl/insert.py
@@ -6,22 +6,23 @@ import dotenv
 import os
 import pymysql
 
-MOVIE_COLUMNS =[
-    'mv_code',"mv_name","altname" ,"playtime" ,"release_date" ,
-    "watched_rating_num" ,"watched_rating" ,"commentor_rating" ,"netizen_rating_num" ,"netizen_rating" ,
-    "director_short" ,"country_short","actor_short","grade_short" ,"poster_url","year"
+MOVIE_COLUMNS = [
+    'mv_code', "mv_name", "altname", "playtime", "release_date",
+    "watched_rating_num", "watched_rating", "commentor_rating", "netizen_rating_num", "netizen_rating",
+    "director_short", "country_short", "actor_short", "grade_short", "poster_url", "year"
 ]
 
-ACTOR_COLUMNS =["ac_code", "ac_name", "ismain", "ac_role", "img_url"]
+ACTOR_COLUMNS = ["ac_code", "ac_name", "ismain", "ac_role", "img_url"]
 
-DIRECTOR_COLUMNS =["dr_code", "dr_name", " img_url"]
+DIRECTOR_COLUMNS = ["dr_code", "dr_name", " img_url"]
+
 
 def init_db():
     dotenv.load_dotenv()
-    SQL_URL=os.getenv("SQL_URL")
-    SQL_DB=os.getenv("SQL_DB")
-    SQL_USER=os.getenv("SQL_USER")
-    SQL_PASSWD=os.getenv("SQL_PASSWD")
+    SQL_URL = os.getenv("SQL_URL")
+    SQL_DB = os.getenv("SQL_DB")
+    SQL_USER = os.getenv("SQL_USER")
+    SQL_PASSWD = os.getenv("SQL_PASSWD")
 
     db = pymysql.connect(
         user=SQL_USER,
@@ -35,37 +36,42 @@ def init_db():
 
     return db, cursor
 
-def insert_movie_param(code,data) -> Tuple or None :
+
+def insert_movie_param(code, data) -> Tuple or None:
     mv = data['mv']
     return (
-        code, # mv_code
+        code,  # mv_code
         mv.get("name"),        # "mv_name"
-        mv.get("altname"),        # "altname" 
-        mv.get("minute"),        # "playtime" 
-        None,        # "release_date" 
-        mv.get("watched_rating_num"),        #"watched_rating_num" 
-        mv.get("watched_rating"),         #"watched_rating" 
-        mv.get("commentor_rating"),         #"commentor_rating" 
-        mv.get("netizen_rating_num"),         #"netizen_rating_num" 
-        mv.get("netizen_rating"),         #"netizen_rating" 
-        mv.get("director_short"),         #"director_short" 
-        None,         #"country_short"
-        None,         #"actor_short"
-        None,         #"grade_short" 
-        data.get['poster'],         #"poster_url"
-        mv.get("year"),         #"year"
+        mv.get("altname"),        # "altname"
+        mv.get("minute"),        # "playtime"
+        None,        # "release_date"
+        mv.get("watched_rating_num"),  # "watched_rating_num"
+        mv.get("watched_rating"),  # "watched_rating"
+        mv.get("commentor_rating"),  # "commentor_rating"
+        mv.get("netizen_rating_num"),  # "netizen_rating_num"
+        mv.get("netizen_rating"),  # "netizen_rating"
+        mv.get("director_short"),  # "director_short"
+        None,  # "country_short"
+        None,  # "actor_short"
+        None,  # "grade_short"
+        data.get['poster'],  # "poster_url"
+        mv.get("year"),  # "year"
     )
 
-def insert_actor_param(code,data)  ->  Tuple or None :
+
+def insert_actor_param(code, data) -> Tuple or None:
     return ()
 
-def insert_director_param(code,data) -> Tuple or None :
+
+def insert_director_param(code, data) -> Tuple or None:
     return ()
 
-def insert_movie_param(code,data) -> Tuple or None :
+
+def insert_movie_param(code, data) -> Tuple or None:
     return ()
 
-def insert_movie_param(code,data) -> Tuple or None :
+
+def insert_movie_param(code, data) -> Tuple or None:
     return ()
 
 
@@ -73,22 +79,17 @@ def main():
     PICKLE_PATH_ROOT = Path("./out")
     for pickle_path in PICKLE_PATH_ROOT.glob("*.pickle"):
         code_start, code_end = pickle_path.name.split('.')[-2].split('_')[:2]
-        code_range = range(int(code_start),int(code_end))
+        code_range = range(int(code_start), int(code_end))
 
-        with open(pickle_path,'rb') as f:
+        with open(pickle_path, 'rb') as f:
             datas = pickle.load(f)
-        
-        for mv_code, data in zip(code_range,datas):
-            
+
+        for mv_code, data in zip(code_range, datas):
+
             if data is None:
                 continue
 
-            
-            print(data) 
-
-    
-
-           
+            print(data)
 
 
 if __name__ == "__main__":
---
+++ sqls/init.sql
@@ -0,0 +1,83 @@
+SET foreign_key_checks = 0;
+DROP TABLE IF EXISTS movie;
+SET foreign_key_checks = 1;
+CREATE TABLE movie (
+	mv_code INT PRIMARY KEY,
+	mv_name TINYTEXT,
+    altname TINYTEXT,
+    playtime INT,
+    release_date DATE,
+    watched_rating_num INT,
+    watched_rating FLOAT,
+    commentor_rating INT,
+    netizen_rating_num INT,
+    netizen_rating FLOAT,
+    director_short TINYTEXT,
+    country_short TINYTEXT,
+    actor_short TEXT,
+    grade_short TEXT,
+    poster_url TEXT
+);
+
+SET foreign_key_checks = 0;
+DROP TABLE IF EXISTS actor;
+SET foreign_key_checks = 1;
+CREATE TABLE actor (
+	ac_code INT PRIMARY KEY,
+	ac_name TINYTEXT,
+	ismain INT DEFAULT NULL,
+    ac_role TINYTEXT,
+    img_url TEXT
+);
+
+SET foreign_key_checks = 0;
+DROP TABLE IF EXISTS director;
+SET foreign_key_checks = 1;
+
+CREATE TABLE director (
+	dr_code INT PRIMARY KEY,
+	dr_name TINYTEXT,
+    img_url TEXT
+);
+
+DROP TABLE IF EXISTS who_directed;
+CREATE TABLE who_directed (
+	mv_code INT NOT NULL,
+    dr_code INT NOT NULL,
+    FOREIGN KEY (mv_code) REFERENCES movie(mv_code)
+		ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    FOREIGN KEY (dr_code) REFERENCES director(dr_code)
+		ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS who_acted;
+CREATE TABLE who_acted (
+	mv_code INT NOT NULL,
+    ac_code INT NOT NULL,
+    FOREIGN KEY (mv_code) REFERENCES movie(mv_code)
+		ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    FOREIGN KEY (ac_code) REFERENCES actor(ac_code)
+		ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS where_made;
+CREATE TABLE where_made (
+	mv_code INT NOT NULL,
+    country TINYTEXT NOT NULL,
+    FOREIGN KEY (mv_code) REFERENCES movie(mv_code)
+		ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS what_grade;
+CREATE TABLE what_grade (
+	mv_code INT NOT NULL,
+    grade TINYTEXT NOT NULL,
+    FOREIGN KEY (mv_code) REFERENCES movie(mv_code)
+		ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
